2132756c95287718cfee6aeaa6d2499c6773201a
Hitesh Bemal <70004109+Hiteshbemal24@users.noreply.github.com>
2023-09-06 16:23:36 +0530

    Create 88. Merge Sorted Array.java

---
+++ 88. Merge Sorted Array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = 0, j = 0;
+        int k = 0;
+        int[] nums3 = new int[m + n];
+
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j]) {
+                nums3[k++] = nums1[i++];
+            } else {
+                nums3[k++] = nums2[j++];
+            }
+        }
+
+        while (i < m) {
+            nums3[k++] = nums1[i++];
+        }
+
+        while (j < n) {
+            nums3[k++] = nums2[j++];
+        }
+
+        for (i = 0; i < m + n; i++) {
+            nums1[i] = nums3[i];
+        }
+    }
+}
